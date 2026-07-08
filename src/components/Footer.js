cfb7fd230ef78c4338b0a6e70b22675fb0d0c406
Sohel Mondal <mondalsohel47025@gmail.com>
2026-07-08 17:39:07 +0530

    sign in page added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-black text-white flex justify-center items-center px-4 h-14'>
-        <p>Copyright &copy; Creators - All rights reserved</p>
+    <footer className='bg-black text-white flex justify-center items-center px-4 h-10'>
+        <p>Copyright &copy; Creatora - All rights reserved</p>
     </footer>
   )
 }
